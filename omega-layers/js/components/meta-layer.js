1c6d3706d25e5714b0e0e59bf7d085d9faca27cd
Scott Carroll <scott_carroll19@yahoo.com>
2021-05-16 10:32:23 -0500

    Update meta-layer.js

--- omega-layers/js/components/meta-layer.js
+++ omega-layers/js/components/meta-layer.js
@@ -11,7 +11,7 @@ Vue.component("meta-layer", {
     computed: {
         showLayersPS: function()
         {
-            return this.metaLayer.getLayersPS().gte(10);
+            return this.metaLayer.getLayersPS().gte(0.5);
         },
         showPowerers: function()
         {
@@ -23,11 +23,11 @@ Vue.component("meta-layer", {
         }
     },
     template: `<div class="meta-layer">
-<p class="resource">You have {{formatNumber(metaLayer.resource, 2, 0, 1e9)}} <resource-name :layerid="metaLayer.layer.floor()"></resource-name></p>
-<p class="resource alpha" v-if="metaLayer.layer.gt(0)">You have approx. {{formatNumber(metaLayer.getApproxAlpha(), 2, 0, 1e9)}} <resource-name :layerid="0"></resource-name></p>
-<p class="layer">You are on Layer {{formatNumber(metaLayer.layer.add(1), 2, 0, 1e12)}}</p>
-<p>Your Resource multiplies by x{{formatNumber(metaLayer.getMultiPS(), 2, 2)}} each second
-<span v-if="showLayersPS"><br/>and thus advancing {{formatNumber(metaLayer.getLayersPS(), 2, 2)}} Layers per second</span></p>
+<p class="resource">You have {{formatNumber(metaLayer.resource, 3, 0, 1e15)}} <resource-name :layerid="metaLayer.layer.floor()"></resource-name></p>
+<p class="resource alpha" v-if="metaLayer.layer.gt(0)">You have approx. {{formatNumber(metaLayer.getApproxAlpha(), 3, 0, 1e15)}} <resource-name :layerid="0"></resource-name></p>
+<p class="layer">You are on Layer {{formatNumber(metaLayer.layer.add(1), 3, 0, 1e15)}}</p>
+<p>Your Resource multiplies by x{{formatNumber(metaLayer.getMultiPS(), 2, 2, 1e15)}} each second
+<span v-if="showLayersPS"><br/>You advance {{formatNumber(metaLayer.getLayersPS(), 2, 2, 1e15)}} layers per second</span></p>
 <button v-if="canMaxAll" @click="metaLayer.maxAll()" class="max-all">Max All (M)</button>
 <h4>Resource Multipliers</h4>
 <upgrade-container :upgrades="metaLayer.multiplierUpgrades"></upgrade-container>
